8954f49bb07c2e52120bc5d23090239d429032d3
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-03-21 22:58:42 +1000

    Its alive....

--- src/Server/ChatterBox.ChatServer/Handlers/BroadcastMessageHandler.cs
+++ src/Server/ChatterBox.ChatServer/Handlers/BroadcastMessageHandler.cs
@@ -1,22 +1,27 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using ChatterBox.MessageContracts.Commands;
 using ChatterBox.MessageContracts.Events;
 using Nimbus;
 using Nimbus.Handlers;
+using Serilog;
+using Serilog.Events;
 
 namespace ChatterBox.ChatServer.Handlers
 {
-    public class MessageHandler : IHandleCommand<BroadcastMessageCommand>
+    public class BroadcastMessageHandler : IHandleCommand<BroadcastMessageCommand>
     {
         private readonly IBus _bus;
 
-        public MessageHandler(IBus bus)
+        public BroadcastMessageHandler(IBus bus)
         {
             _bus = bus;
         }
 
         public async Task Handle(BroadcastMessageCommand message)
         {
+            Log.Debug("{message}", message);
+
             await _bus.Publish(new MessageReceivedEvent(message.ClientId, message.Message));
         }
     }
