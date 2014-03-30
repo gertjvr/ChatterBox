0a92b9bfb30a301ca937b38ff4fd039a4fa53f00
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-03-30 19:02:23 +1000

    Added Sample Messanger.Console Application.

---
+++ src/Messanger.Console/IoC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Autofac;
+using Autofac.Builder;
+using ChatterBox.Core.Infrastructure;
+using Domain.Aggregates.ContactAggregate;
+
+namespace Messanger.Console
+{
+    internal static class IoC
+    {
+        [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Io")]
+        public static IContainer LetThereBeIoC(ContainerBuildOptions containerBuildOptions = ContainerBuildOptions.None, Action<ContainerBuilder> preHooks = null)
+        {
+            var builder = new ContainerBuilder();
+
+            var thisAssembly = typeof(IoC).Assembly;
+            var coreAssembly = typeof(SystemClock).Assembly;
+            var domainAssembly = typeof (Contact).Assembly;
+
+            builder.RegisterAssemblyModules(
+                thisAssembly, 
+                coreAssembly, 
+                domainAssembly);
+
+            if (preHooks != null) preHooks(builder);
+
+            return builder.Build(containerBuildOptions);
+        }
+    }
+}
---
+++ src/Messanger.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Autofac;
+using ChatterBox.Core.Persistence;
+using Domain.Aggregates.ContactAggregate;
+
+namespace Messanger.Console
+{
+    public class Program
+    {
+        private static IContainer _container;
+
+        public static void Main(string[] args)
+        {
+            _container = IoC.LetThereBeIoC();
+
+            var fred = CreateContact("fred");
+            var wilma = CreateContact("wilma");
+
+
+        }
+
+        private static Guid CreateContact(string username)
+        {
+            using (var scope = _container.BeginLifetimeScope())
+            {
+                var uow = scope.Resolve<IUnitOfWork>();
+                var repo = scope.Resolve<IRepository<Contact>>();
+                var contact = new Contact(username);
+                repo.Add(contact);
+                uow.Complete();
+
+                return contact.Id;
+            }
+        }
+    }
+}
