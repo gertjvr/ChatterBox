9498ecbf5c92ecaa743d176e24197f326645a139
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-04-11 21:07:34 +1000

    Implemented Users Allowed Rooms Handler.

--- src/Server/ChatterBox.ChatServer/Handlers/AllowedRoomsRequestHandler.cs
+++ src/Server/ChatterBox.ChatServer/Handlers/AllowedRoomsRequestHandler.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Autofac.Features.OwnedInstances;
+using ChatterBox.Core.Mapping;
 using ChatterBox.Core.Persistence;
+using ChatterBox.Domain.Aggregates.RoomAggregate;
+using ChatterBox.Domain.Queries;
+using ChatterBox.MessageContracts.Dtos;
 using ChatterBox.MessageContracts.Requests;
 
 namespace ChatterBox.ChatServer.Handlers
 {
     public class AllowedRoomsRequestHandler : ScopedRequestHandler<AllowedRoomsRequest, AllowedRoomsResponse>
     {
-        public AllowedRoomsRequestHandler(Func<Owned<IUnitOfWork>> unitOfWork) : base(unitOfWork)
+        private readonly IMapToNew<Room, RoomDto> _mapper;
+
+        public AllowedRoomsRequestHandler(
+            Func<Owned<IUnitOfWork>> unitOfWork,
+            IMapToNew<Room, RoomDto> mapper) 
+            : base(unitOfWork)
         {
+            _mapper = mapper;
         }
 
-        public override Task<AllowedRoomsResponse> Execute(IUnitOfWork context, AllowedRoomsRequest request)
+        public override async Task<AllowedRoomsResponse> Execute(IUnitOfWork context, AllowedRoomsRequest request)
         {
-            throw new NotImplementedException();
+            var repository = context.Repository<Room>();
+
+            var rooms = repository.Query(new UserAllowedRoomsQuery(request.UserId));
+
+            return new AllowedRoomsResponse(rooms.Select(_mapper.Map));
         }
     }
 }
---
+++ src/Shared/ChatterBox.Domain/Queries/UserAllowedRoomsQuery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using ChatterBox.Core.Infrastructure.Queries;
+using ChatterBox.Domain.Aggregates.RoomAggregate;
+
+namespace ChatterBox.Domain.Queries
+{
+    public class UserAllowedRoomsQuery : IQuery<Room>
+    {
+        private readonly Guid _userId;
+
+        public UserAllowedRoomsQuery(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IQueryable<Room> Execute(IQueryable<Room> source)
+        {
+            return source.Where(room => room.Contacts.Contains(_userId));
+        }
+    }
+}
