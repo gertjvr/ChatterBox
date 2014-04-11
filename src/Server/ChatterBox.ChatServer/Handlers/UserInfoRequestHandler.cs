80c2386664b11099c8c04172090ed104ca628658
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-04-11 21:32:56 +1000

    Implemented UserInfo Handler.

--- src/Server/ChatterBox.ChatServer/Handlers/UserInfoRequestHandler.cs
+++ src/Server/ChatterBox.ChatServer/Handlers/UserInfoRequestHandler.cs
@@ -1,20 +1,33 @@
 using System;
 using System.Threading.Tasks;
 using Autofac.Features.OwnedInstances;
+using ChatterBox.Core.Mapping;
 using ChatterBox.Core.Persistence;
+using ChatterBox.Domain.Aggregates.UserAggregate;
+using ChatterBox.MessageContracts.Dtos;
 using ChatterBox.MessageContracts.Requests;
 
 namespace ChatterBox.ChatServer.Handlers
 {
     public class UserInfoRequestHandler : ScopedRequestHandler<UserInfoRequest, UserInfoResponse>
     {
-        public UserInfoRequestHandler(Func<Owned<IUnitOfWork>> unitOfWork) : base(unitOfWork)
+        private readonly IMapToNew<User, UserDto> _mapper;
+
+        public UserInfoRequestHandler(
+            Func<Owned<IUnitOfWork>> unitOfWork,
+            IMapToNew<User, UserDto> mapper) 
+            : base(unitOfWork)
         {
+            _mapper = mapper;
         }
 
-        public override Task<UserInfoResponse> Execute(IUnitOfWork context, UserInfoRequest request)
+        public override async Task<UserInfoResponse> Execute(IUnitOfWork context, UserInfoRequest request)
         {
-            throw new NotImplementedException();
+            var repository = context.Repository<User>();
+
+            var user = repository.GetById(request.UserId);
+
+            return new UserInfoResponse(_mapper.Map(user));
         }
     }
 }
