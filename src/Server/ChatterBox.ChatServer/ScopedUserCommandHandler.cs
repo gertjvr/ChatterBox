9e3cdcdc5a3a72f793155dd81d9cd88380798624
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-04-12 21:12:35 +1000

    Few updates to ChatServer.

---
+++ src/Server/ChatterBox.ChatServer/ScopedUserCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChatterBox.Core.Persistence;
+using ChatterBox.Domain.Aggregates.UserAggregate;
+using ChatterBox.Domain.Extensions;
+using ChatterBox.MessageContracts.Commands;
+using Nimbus.Handlers;
+
+namespace ChatterBox.ChatServer
+{
+    public abstract class ScopedUserCommandHandler<TBusCommand> : IHandleCommand<TBusCommand> where TBusCommand : IUserCommand
+    {
+        private readonly Func<IUnitOfWork> _unitOfWork;
+
+        public ScopedUserCommandHandler(Func<IUnitOfWork> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(TBusCommand command)
+        {
+            using (var context = _unitOfWork())
+            {
+                try
+                {
+                    var repository = context.Repository<User>();
+                    var user = repository.VerifyUser(command.UserId);
+
+                    await Execute(context, user, command);
+                }
+                catch
+                {
+                    context.Abandon();
+                    throw;
+                }
+            }
+        }
+
+        public abstract Task Execute(IUnitOfWork context, User callingUser, TBusCommand command);
+    }
+}
---
+++ src/Shared/ChatterBox.Domain/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ChatterBox.Core.Persistence;
+using ChatterBox.Domain.Aggregates.RoomAggregate;
+using ChatterBox.Domain.Aggregates.UserAggregate;
+using ThirdDrawer.Extensions.StringExtensionMethods;
+
+namespace ChatterBox.Domain.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static User VerifyUser(this IRepository<User> repository, Guid userId)
+        {
+            var user = repository.GetById(userId);
+
+            if (user == null)
+            {
+                throw new Exception("Unable to find {0}.".FormatWith(userId));
+            }
+
+            return user;
+        }
+
+        public static Room VerifyRoom(this IRepository<Room> repository, Guid roomId, bool mustBeOpen = false)
+        {
+            var room = repository.GetById(roomId);
+
+            if (room == null)
+            {
+                throw new Exception("Unable to find {0}.".FormatWith(roomId));
+            }
+
+            if (room.Closed && mustBeOpen)
+            {
+                throw new Exception("{0} is closed.".FormatWith(room.Name));
+            }
+
+            return room;
+        }
+    }
+}
