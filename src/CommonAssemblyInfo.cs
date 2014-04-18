7384e75984d8a1c171dfe67bcd538853ca8c1a92
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-04-18 17:05:39 +1000

    Updated packages
    Removed nuget restore.. my it rest in pieces!

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyProduct("Fixie")]
+[assembly: AssemblyProduct("ChatterBox")]
 [assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
-[assembly: AssemblyCopyright("Copyright (c) 2013-2014 Patrick Lioi")]
-[assembly: AssemblyCompany("Patrick Lioi")]
-[assembly: AssemblyDescription("A convention-based test framework.")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 Gert Jansen van Rensburg")]
+[assembly: AssemblyCompany("Gert Jansen van Rensburg")]
+[assembly: AssemblyDescription("Chat Service using NimbusAPI")]
 [assembly: AssemblyConfiguration("Release")]
