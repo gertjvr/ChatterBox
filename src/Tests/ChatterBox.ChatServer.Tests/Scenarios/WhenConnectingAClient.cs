375feb40cd636ef5a15c4e954ef560c906b67e9f
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-04-21 21:26:44 +1000

    Fixed tests.

--- src/Tests/ChatterBox.ChatServer.Tests/Scenarios/WhenConnectingAClient.cs
+++ src/Tests/ChatterBox.ChatServer.Tests/Scenarios/WhenConnectingAClient.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ChatterBox.ChatServer.Handlers.Users;
+using ChatterBox.Core.Infrastructure;
 using ChatterBox.Core.Tests.Specifications;
+using ChatterBox.Domain.Aggregates.UserAggregate;
 using ChatterBox.MessageContracts.Users.Requests;
+using NSubstitute;
 using Ploeh.AutoFixture;
 using Shouldly;
 
@@ -10,12 +13,20 @@ namespace ChatterBox.ChatServer.Tests.Scenarios
 {
     public class WhenConnectingAClient : AutoSpecificationForAsync<CreateClientRequestHandler>
     {
+        protected User CallingUser;
+
         protected CreateClientRequest Request;
         protected CreateClientResponse Response;
 
         protected override async Task<CreateClientRequestHandler> Given()
         {
-            Request = Fixture.Create<CreateClientRequest>();
+            CallingUser = Fixture.Create<User>();
+            
+            Request = new CreateClientRequest(Fixture.Create<Guid>(), Fixture.Create<string>(), CallingUser.Id);
+
+            var repository = Fixture.Freeze<IRepository<User>>();
+            repository.GetById(Arg.Is(CallingUser.Id))
+                .Returns(CallingUser);
 
             return Fixture.Create<CreateClientRequestHandler>();
         }
